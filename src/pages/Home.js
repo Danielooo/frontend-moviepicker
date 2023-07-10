aa32c172411fc9bb26592e9eefd2d58fdd19f3a2
Danielooo <dvanraffe@gmail.com>
2023-07-10 10:42:31 +0200

    created basic navbar with page links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 function Home() {
 
-    return (
-        <>
-            <h1>Home page</h1>
+  return (
+      <>
+          <h1>Home page</h1>
+
+          {/*Movie filters*/}
+
+          {/*Movie selection*/}
+
+          {/*Movie shortlist*/}
         </>
-    )
+  )
 }
+
+export default Home;
