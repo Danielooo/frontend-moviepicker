adfbcdf58a3ebb7506e5e8b866b752e31b48c992
Danielooo <dvanraffe@gmail.com>
2023-09-05 09:26:27 +0200

    added poster not found

--- src/components/moviecard/MovieCard.js
+++ src/components/moviecard/MovieCard.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import './MovieCard.css';
 import './../popup/PopUp.css';
 import './../infobutton/InfoButton.css';
+import posterNotFound from './../../assets/images/404-poster-not-found.svg'
 
 function MovieCard({movieKey, movie, handleAddToShortlist, isMovieInShortlist}) {
 
-  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+  const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
+  const [ posterImage, setPosterImage ] = useState(null);
+
+
+
+  useEffect(() => {
+    async function checkPosterUrl() {
+      try {
+        const response = await axios.get(posterUrl)
+
+
+        if (response.status === 200) {
+          setPosterImage(<img className='movie-poster' src={posterUrl} alt='poster not found'/>)
+          console.log('response data when 200: ', response)
+        } else {
+          setPosterImage(<img className='movie-poster' src={posterNotFound}  alt='poster not found'/>)
+          console.log('response data when else: ', response)
+
+        }
+      } catch (e) {
+        setPosterImage(<img className='movie-poster' src={posterNotFound}  alt='poster not found'/>)
+        console.log('error when catch: ')
+        console.error(e)
+      }
+    }
+
+    void checkPosterUrl()
+
+  }, [posterUrl, posterNotFound]);
+
+
 
   return (
     <div key={movieKey} className='movie-card'>
       <div className='movie-card-top'>
 
         <div className='movie-poster-container'>
-          <img className='movie-poster' src={posterUrl} alt='poster not found'/>
+          {posterImage}
           <div className='popup-container'>
             <div className='popup-text'>
               <p>{movie.overview}</p>
