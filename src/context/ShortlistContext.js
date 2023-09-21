d23c408c4a5d84db22b82e51982dedceea5fefd4
Danielooo <dvanraffe@gmail.com>
2023-09-21 14:05:58 +0200

    sections-container space-between error

--- src/context/ShortlistContext.js
+++ src/context/ShortlistContext.js
@@ -1,24 +1,77 @@
-import React, { createContext, useState } from 'react'
+import React, {createContext, useEffect, useState} from 'react'
 
 
 export const ShortlistContext = createContext(null);
 
-function ShortlistContextProvider({ children }) {
-  const [ shortlist, setShortlist ] = useState([]);
-
-  const shortlistActions = {
-    shortlist,
-    setShortlist,
-  }
-
-
-
-
-  return (
-    <ShortlistContext.Provider value={ shortlistActions }>
-      {children}
+function ShortlistContextProvider({children}) {
+    const [shortlist, setShortlist] = useState([]);
+    const [afterMount, setAfterMount] = useState(false)
+    
+    
+    useEffect(() => {
+        // checks if shortlist exists. If not, declares shortlist in localStorage and sets shortlist to empty array
+        try {
+            if (localStorage.getItem('localStorageShortlist')) {
+                const arrayAsString = localStorage.getItem('localStorageShortlist')
+                const parsedShortlist = JSON.parse(arrayAsString);
+                
+                console.log('parsedShortlist : ', parsedShortlist)
+                setShortlist(parsedShortlist)
+                setAfterMount(true);
+                
+            } else {
+                const emptyArray = [];
+                localStorage.setItem('localStorageShortlist', JSON.stringify(emptyArray))
+                setShortlist(emptyArray)
+                console.log('localStorageShortlist: ', shortlist)
+            }
+        } catch (e) {
+            console.error('error retrieving shortlist from localStorage: ', e);
+        }
+        
+    }, [])
+    
+    // Every time shortlist state is updated >> localStorageShortlist is updated as well
+    useEffect(() => {
+        if (afterMount) {
+            shortlistToLocalStorageShortlist()
+        }
+    }, [shortlist]);
+    
+    
+    function getLocalStorageShortlist() {
+        return localStorage.getItem('localStorageShortlist')
+    }
+    
+    function shortlistToLocalStorageShortlist() {
+        const updatedShortlist = JSON.stringify(shortlist);
+        
+        localStorage.setItem('localStorageShortlist', updatedShortlist)
+    }
+    
+    function localStorageShortlistToEmptyArray() {
+        localStorage.setItem('localStorageShortlist', [])
+    }
+    
+    function clearShortlistAndLocalStorageShortlist() {
+        setShortlist([])
+        localStorageShortlistToEmptyArray()
+        console.log('clear shortlist invoked')
+    }
+    
+    
+    const shortlistActions = {
+        shortlist,
+        setShortlist,
+        clearShortlistAndLocalStorageShortlist,
+    }
+    
+    
+    return (
+    <ShortlistContext.Provider value={shortlistActions}>
+        {children}
     </ShortlistContext.Provider>
-  )
+    )
 }
 
 export default ShortlistContextProvider;
--- src/pages/moviesearch/MovieSearch.js
+++ src/pages/moviesearch/MovieSearch.js
@@ -23,235 +23,231 @@ import Button from "../../components/button/Button";
 
 
 function MovieSearch() {
-  const navigate = useNavigate();
-
-  // Actor search
-  const [actorName, setActorName] = useState('');
-  const [actorId, setActorId] = useState(0)
-  const [errorActor, toggleErrorActor] = useState(false)
-
-  // Genre search
-  const [genreAndIdListOfApi, setGenreAndIdListOfApi] = useState([])
-  const [genreChoice, setGenreChoice] = useState('');
-  const [genreChoiceId, setGenreChoiceId] = useState(0)
-  const [errorGenreList, toggleErrorGenreList] = useState(false)
-  const [errorGenre, toggleErrorGenre] = useState(false)
-
-  // Decade search
-  const [errorDecade, toggleErrorDecade] = useState(false)
-  const [selectedDecade, setSelectedDecade] = useState('2020s');
-  const decades = [
-    '2020s', '2010s', '2000s', '1990s', '1980s', '1970s', '1960s',
-    '1950s', '1940s', '1930s', '1920s', '1910s', '1900s'
-  ];
-
-  // Misc
-  const [loading, toggleLoading] = useState(false);
-  const [movies, setMovies] = useState([]);
-  const {shortlist, setShortlist} = useContext(ShortlistContext);
-
-  // Api endpoint header
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN_ACTOR}`,
-    },
-  };
-
-
-  //  =========================
-  //  ===  FUNCTIES  ACTOR  ===
-  //  =========================
-
-
-  useEffect(() => {
-    toggleLoading(true)
-
-    if (actorId) {
-      void getMoviesByActorId(toggleErrorActor, toggleLoading, setMovies, options, actorId)
+    const navigate = useNavigate();
+    
+    // Actor search
+    const [actorName, setActorName] = useState('');
+    const [actorId, setActorId] = useState(0)
+    const [errorActor, toggleErrorActor] = useState(false)
+    
+    // Genre search
+    const [genreAndIdListOfApi, setGenreAndIdListOfApi] = useState([])
+    const [genreChoice, setGenreChoice] = useState('');
+    const [genreChoiceId, setGenreChoiceId] = useState(0)
+    const [errorGenreList, toggleErrorGenreList] = useState(false)
+    const [errorGenre, toggleErrorGenre] = useState(false)
+    
+    // Decade search
+    const [errorDecade, toggleErrorDecade] = useState(false)
+    const [selectedDecade, setSelectedDecade] = useState('2020s');
+    const decades = [
+        '2020s', '2010s', '2000s', '1990s', '1980s', '1970s', '1960s',
+        '1950s', '1940s', '1930s', '1920s', '1910s', '1900s'
+    ];
+    
+    // Misc
+    const [loading, toggleLoading] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const {shortlist, setShortlist} = useContext(ShortlistContext);
+    
+    // Api endpoint header
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN_ACTOR}`,
+        },
+    };
+    
+    
+    //  =========================
+    //  ===  FUNCTIES  ACTOR  ===
+    //  =========================
+    
+    
+    useEffect(() => {
+        toggleLoading(true)
+        
+        if (actorId) {
+            void getMoviesByActorId(toggleErrorActor, toggleLoading, setMovies, options, actorId)
+        }
+        toggleLoading(false)
+        
+    }, [actorId])
+    
+    
+    //  =========================
+    //  ===  FUNCTIES GENRE  ====
+    //  =========================
+    
+    // Mount only
+    useEffect(() => {
+        void getGenresAndIdsOfApi(setGenreAndIdListOfApi, toggleErrorGenreList, options)
+        
+    }, [])
+    
+    
+    useEffect(() => {
+        
+        if (genreChoiceId !== undefined) {
+            void getMoviesByGenreId(setMovies, toggleLoading, toggleErrorGenre, genreChoiceId, options)
+        } else {
+            // TODO uitzondering bedenken of else verwijderen
+        }
+        
+        toggleLoading(false)
+    }, [genreChoiceId])
+    
+    
+    // =========================
+    // ===  FUNCTIES DECADE  ===
+    // =========================
+    
+    // In helper DecadeSearch.js
+    
+    
+    //  ========================
+    //  ===  HANDLE SUBMITS  ===
+    //  ========================
+    
+    function handleActorSubmit(e) {
+        e.preventDefault();
+        void getActorIdByName(toggleErrorActor, toggleLoading, setActorId, options, actorName)
     }
-    toggleLoading(false)
-
-  }, [actorId])
-
-
-  //  =========================
-  //  ===  FUNCTIES GENRE  ====
-  //  =========================
-
-  // Mount only
-  useEffect(() => {
-    void getGenresAndIdsOfApi(setGenreAndIdListOfApi, toggleErrorGenreList, options)
-
-  }, [])
-
-
-  useEffect(() => {
-
-    if (genreChoiceId !== undefined) {
-      void getMoviesByGenreId(setMovies, toggleLoading, toggleErrorGenre, genreChoiceId, options)
-    } else {
-      // TODO uitzondering bedenken of else verwijderen
+    
+    function handleGenreSubmit(e) {
+        e.preventDefault();
+        void getGenreIdByInput(toggleLoading, toggleErrorGenre, setGenreChoiceId, genreAndIdListOfApi, genreChoice);
     }
-
-    toggleLoading(false)
-  }, [genreChoiceId])
-
-
-  // =========================
-  // ===  FUNCTIES DECADE  ===
-  // =========================
-
-  // In helper DecadeSearch.js
-
-
-  //  ========================
-  //  ===  HANDLE SUBMITS  ===
-  //  ========================
-
-  function handleActorSubmit(e) {
-    e.preventDefault();
-    void getActorIdByName(toggleErrorActor, toggleLoading, setActorId, options, actorName)
-  }
-
-  function handleGenreSubmit(e) {
-    e.preventDefault();
-    void getGenreIdByInput(toggleLoading, toggleErrorGenre, setGenreChoiceId, genreAndIdListOfApi, genreChoice);
-  }
-
-  function handleDecadeSubmit(e) {
-    e.preventDefault()
-    void getMoviesByDecade(setMovies, selectedDecade, toggleLoading, toggleErrorDecade, options);
-  }
-
-  function handleClickRandomize(e) {
-    e.preventDefault()
-    navigate('/wheel')
-  }
-
-  function handleClearShortlist(e) {
-    e.preventDefault()
-    setShortlist([])
-  }
-
-  // ===================
-  // ===  SHORTLIST  ===
-  // ===================
-
-  // >>>  helper  <<<
-  function handleAddToShortlist(movie) {
-    setShortlist((prevShortlist) => [...prevShortlist, movie]);
-    setMovies((prevMovies) =>
-      prevMovies.map((prevMovie) =>
+    
+    function handleDecadeSubmit(e) {
+        e.preventDefault()
+        void getMoviesByDecade(setMovies, selectedDecade, toggleLoading, toggleErrorDecade, options);
+    }
+    
+    function handleClickRandomize(e) {
+        e.preventDefault()
+        navigate('/wheel')
+    }
+    
+    function handleClearShortlist(e) {
+        e.preventDefault()
+        setShortlist([])
+    }
+    
+    // ===================
+    // ===  SHORTLIST  ===
+    // ===================
+    
+    // >>>  helper  <<<
+    function handleAddToShortlist(movie) {
+        setShortlist((prevShortlist) => [...prevShortlist, movie]);
+        setMovies((prevMovies) =>
+        prevMovies.map((prevMovie) =>
         prevMovie.id === movie.id ? {...prevMovie, isAdded: true} : prevMovie
-      )
-    );
-  }
-
-  function handleRemoveFromShortlist(movie) {
-    setShortlist((prevShortlist) =>
-      prevShortlist.filter((prevMovie) => prevMovie.id !== movie.id)
-    );
-    setMovies((prevMovies) =>
-      prevMovies.map((prevMovie) =>
+        )
+        );
+    }
+    
+    function handleRemoveFromShortlist(movie) {
+        setShortlist((prevShortlist) =>
+        prevShortlist.filter((prevMovie) => prevMovie.id !== movie.id)
+        );
+        setMovies((prevMovies) =>
+        prevMovies.map((prevMovie) =>
         prevMovie.id === movie.id ? {...prevMovie, isAdded: false} : prevMovie
-      )
-    );
-  }
-
-  function isMovieInShortlist(movieId) {
-    return shortlist.some((item) => item.id === movieId);
-  }
-
-  //  ================
-  //  ===  RETURN  ===
-  //  ================
-
-
-  return (
-    <div className='main-outer-container'>
-      <div className='main-inner-container'>
-        <div className='sections-container'>
-
-          {/*Movie Search  */}
-
-          <section className='section-outer-container'>
-            <div className='movie-search section-inner-container'>
-              <div className='title-and-infobutton-line'>
-                <h2 className='section-title'>Movie Search</h2>
-
-                <InfoButton
-                  text={`You can search on Actor, Genre and Decade.\nCombining search queries is not possible.\nThe results in Movie Selection are the 20 best rated movies that have a minimum of 200 votes`}
-                />
-              </div>
-
-              <SearchOnActor
+        )
+        );
+    }
+    
+    function isMovieInShortlist(movieId) {
+        return shortlist.some((item) => item.id === movieId);
+    }
+    
+    //  ================
+    //  ===  RETURN  ===
+    //  ================
+    
+    
+    return (
+    
+    // TODO: build css from the ground up. Only Nav, Footer and part global.css is uncommented
+    
+    <main className="main-container">
+        <div className="sections-container">
+            
+            {/*Movie Search  */}
+            
+            <section className='section-container'>
+                <div className='title-and-infobutton-line'>
+                    <h2 className='section-title'>Movie Search</h2>
+                    
+                    <InfoButton
+                    text={`You can search on Actor, Genre and Decade.\nCombining search queries is not possible.\nThe results in Movie Selection are the 20 best rated movies that have a minimum of 200 votes`}
+                    />
+                </div>
+                
+                <SearchOnActor
                 handleActorSubmit={handleActorSubmit}
                 actorName={actorName}
                 setActorName={setActorName}
                 errorActor={errorActor}
-              />
-
-
-              {/*  GENRE  */}
-
-              <SearchOnGenre
+                />
+                
+                
+                {/*  GENRE  */}
+                
+                <SearchOnGenre
                 genreAndIdListOfApi={genreAndIdListOfApi}
                 errorGenreList={errorGenreList}
                 errorGenre={errorGenre}
                 handleGenreSubmit={handleGenreSubmit}
                 genreChoice={genreChoice}
                 setGenreChoice={setGenreChoice}
-              />
-
-              {/*  DECADE  */}
-
-              <SearchOnDecade
+                />
+                
+                {/*  DECADE  */}
+                
+                <SearchOnDecade
                 handleDecadeSubmit={handleDecadeSubmit}
                 selectedDecade={selectedDecade}
                 setSelectedDecade={setSelectedDecade}
                 decades={decades}
                 errorDecade={errorDecade}
-              />
-            </div>
-          </section>
-
-          {/*Shortlist*/}
-
-          <section className='shortlist-outer-container section-outer-container'>
-            <div className='shortlist-inner-container section-inner-container'>
-              <h2 className='section-title'>Shortlist</h2>
-              <div className='shortlist-movies-container'>
-                <ShortList
-                  shortlist={shortlist}
-                  handleRemoveFromShortlist={handleRemoveFromShortlist}
                 />
-              </div>
-              <Button text='Randomize' handleClick={handleClickRandomize} disabled={false}/>
-              <Button text='Clear Shortlist' handleClick={handleClearShortlist} disabled={false} />
-            </div>
-          </section>
-
+            </section>
+            
+            {/*Shortlist*/}
+            
+            <section className='section-container'>
+                <h2 className='section-title'>Shortlist</h2>
+                <div className='shortlist-movies-container'>
+                    <ShortList handleRemoveFromShortlist={handleRemoveFromShortlist}
+                    />
+                </div>
+            
+            </section>
         </div>
+        
+        
         {/*end sections-container*/}
-
-
-        <div className='sections-container'>
-          <section className='movie-selection section-outer-container'>
+        
+        
+        <section className='movie-selection section-outer-container'>
             <div className='section-inner-container'>
-              <h1 className='section-title'>Movie Selection</h1>
-              <MovieSelection
+                <h1 className='section-title'>Movie Selection</h1>
+                <MovieSelection
                 loading={loading}
                 movies={movies}
                 handleAddToShortlist={handleAddToShortlist}
                 isMovieInShortlist={isMovieInShortlist}/>
             </div>
-          </section>
-        </div>
-      </div>
-    </div>
-  )
+        </section>
+    
+    </main>
+    
+    
+    )
 }
 
 export default MovieSearch;
