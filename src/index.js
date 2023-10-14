d22d4380580b5d41d3f7bb91a173d9baf1ce7f99
Danielooo <dvanraffe@gmail.com>
2023-10-14 21:54:11 +0200

    no node_modules pushed

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from "react-router-dom";
+import App from './App';
+import AuthContextProvider from "./context/AuthContext";
+import ShortlistContextProvider from "./context/ShortlistContext";
+import FavoritesContextProvider from "./context/FavoritesContext";
+import MoviesContextProvider from "./context/MoviesContext";
+
+// styles
+import './styles/global.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+	<Router>
+		<AuthContextProvider>
+			<FavoritesContextProvider>
+				<ShortlistContextProvider>
+					<MoviesContextProvider>
+						<App/>
+					</MoviesContextProvider>
+				</ShortlistContextProvider>
+			</FavoritesContextProvider>
+		</AuthContextProvider>
+	</Router>
+</React.StrictMode>
+);
+
+
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './About.css';
 
 
-
-
-
 function About() {
   
 
@@ -14,7 +11,7 @@ function About() {
 
           <h1 className='login-title'>About</h1>
 
-          <p>Movie Picker is a product of Movie Picker Limited<br />
+          <p>Movie Picker is a product of Movie Picker Limited.<br />
           When you want to watch a movie but don't know which one...<br />
           </p>
           <a className='link-to-moviesearch' href='/'>Try Movie Picker!</a>
